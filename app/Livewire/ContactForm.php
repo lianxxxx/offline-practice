2318310b0d1329ea073c2485ff1ed1cc78c00bca
lianxxxx <leyannemaemayo21@gmail.com>
2026-04-18 19:44:22 +0800

    add Livewire ContactForm component

---
+++ app/Livewire/ContactForm.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ContactForm extends Component
+{
+    public $name = '';
+    public $email = '';
+    public $message = '';
+
+    public function render()
+    {
+        return view('livewire.contact-form');
+    }
+}
